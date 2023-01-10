fefe6c90969e994348f11791b65271bf9e8902b3
AkoviMensah <abmensah@wisc.edu>
2023-01-10 16:31:42 -0500

    Added EF Core and ReviewContext

---
+++ API/API/Data/ReviewContext.cs
@@ -0,0 +1,13 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class ReviewContext : DbContext
+    {
+        public DbSet<Review> Reviews { get; set; }
+        public ReviewContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,7 +1,10 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<ReviewContext>(options => options.UseInMemoryDatabase("ReviewDB"));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
