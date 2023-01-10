ae31c6cf341a673a75ee22384dee0ad1a92494cd
AkoviMensah <abmensah@wisc.edu>
2023-01-10 16:15:03 -0500

    Added Review class

---
+++ API/API/Entities/Review.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Entities
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public int Rating { get; set; }
+        public string Text { get; set; }
+
+    }
+}
